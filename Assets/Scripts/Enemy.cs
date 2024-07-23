36c22f193f579ce75781a9336f691e888c361638
Y2Koi <mkhoit00@gmail.com>
2024-07-22 22:05:39 -0700

    Main updates
    - Added Player movement
    - Added Basic Enemy Movement

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    protected Rigidbody enemyRb;
+    protected GameObject player;
+
+    protected Vector3 forwardDirection;
+
+    public virtual void FollowPlayer()
+    {
+        // Calculate the direction to the player
+        Vector3 directionToPlayer = player.transform.position - transform.position;
+        directionToPlayer.x = 0; // Ignore vertical difference
+
+        // Check if the enemy is within the stop distance
+        if (transform.position.z >= -16.5f)
+        {
+            // Move towards the player
+            transform.position = Vector3.MoveTowards(transform.position, player.transform.position, Time.deltaTime);
+            forwardDirection = directionToPlayer.normalized; // Update forward direction
+        }
+        else
+        {
+            // Move forward in the last known direction
+            transform.position += forwardDirection * Time.deltaTime;
+        }
+
+        if (transform.position.z < -22)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/RedEnemy.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RedEnemy : Enemy
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyRb = GetComponent<Rigidbody>();
+        player = GameObject.Find("Player");
+    }
+
+    public override void FollowPlayer()
+    {
+        // Calculate the direction to the player
+        Vector3 directionToPlayer = player.transform.position - transform.position;
+        directionToPlayer.x = 0; // Ignore vertical difference
+
+        // Check if the enemy is within the stop distance
+        if (transform.position.z >= -16.5f)
+        {
+            // Move towards the player
+            transform.position = Vector3.MoveTowards(transform.position, player.transform.position, 5f * Time.deltaTime);
+            forwardDirection = directionToPlayer.normalized; // Update forward direction
+        }
+        else
+        {
+            // Move forward in the last known direction
+            transform.position += forwardDirection * 5f * Time.deltaTime;
+        }
+
+        if (transform.position.z < -22)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FollowPlayer();
+    }
+}
